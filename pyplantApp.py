15f9ba43ab14080b294482cfbce6cc9596696045
Petteri <42471379+Sim30n@users.noreply.github.com>
2021-08-12 23:03:43 +0300

    arduino class update

--- pyplantApp.py
+++ pyplantApp.py
@@ -8,7 +8,11 @@ class ArduinoBoard:
     time.sleep(2)
 
     def __init__(self):
-        self.humidity = ""
+        self.humidity = 0
+        self.temperature = 0
+        self.water_level = 0
+        self.soil_moisture = 0
+        self.water_tank_level = 0
 
     def read_sensors(self):
         self.ser.write(str.encode("read_sensors"))
@@ -16,7 +20,16 @@ class ArduinoBoard:
         result = self.ser.readline().decode("utf-8")
         print(result)
         #ser.close()
-
+        
+    def run_water_pump(self, pump, position):
+        pass
+    
+    def switch_light(self, position):
+        pass
+    
+    def save_sensor_values(self):
+        pass
+    
 arduino = ArduinoBoard()
 
 arduino.read_sensors()
